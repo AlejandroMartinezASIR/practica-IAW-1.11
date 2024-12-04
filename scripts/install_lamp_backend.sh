6cd6a52ace28904552276eec5d12d520aab06bd8
John Doe <johndoe@example.com>
2024-12-04 08:52:22 +0000

    commit

--- scripts/install_lamp_backend.sh
+++ scripts/install_lamp_backend.sh
@@ -9,11 +9,11 @@ source .env
 
 apt-get install mysql-server -y
 
-sed -i "s/127.0.0.1/$MYSQL_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/127.0.0.1/$SERVER_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-sudo mysql -u root <<< "DROP USER IF EXISTS '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL';"
-sudo mysql -u root <<< "CREATE USER '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
-sudo mysql -u root <<< "GRANT ALL PRIVILEGES ON \`$WORDPRESS_DB_NAME\`.* TO '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL';"
+sudo mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$FRONTEND_PRIVATE_IP;"
+sudo mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$FRONTEND_PRIVATE_IP IDENTIFIED BY $WORDPRESS_DB_PASSWORD;"
+sudo mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$FRONTEND_PRIVATE_IP;"
 sudo mysql -u root <<< "FLUSH PRIVILEGES;"
 
 systemctl restart mysql
